640e2638b6a49050560ae7377b9f893357d9a6b6
Rodrigo Saboya <saboya@users.noreply.github.com>
2019-05-09 23:50:36 -0300

    feature: adding Migrate extension method to run migrations when the application runs

--- DevelopersChallenge2Api/Program.cs
+++ DevelopersChallenge2Api/Program.cs
@@ -7,14 +7,27 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevelopersChallenge2Api
 {
-    public class Program
+    public static class Program
     {
+        public static IWebHost Migrate(this IWebHost webhost)
+        {
+            using (var scope = webhost.Services.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                using (var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDatabaseContext>())
+                {
+                    dbContext.Database.Migrate();
+                }
+            }
+            return webhost;
+        }
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Migrate().Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
