5d3cc6cd8c6d4d362f4e0fbadf87f884d9795d29
Rodrigo Saboya <saboya@users.noreply.github.com>
2019-05-11 23:22:56 -0300

    feature: updating Transaction model

--- DevelopersChallenge2Api/ApplicationDatabaseContext.cs
+++ DevelopersChallenge2Api/ApplicationDatabaseContext.cs
@@ -1,12 +1,20 @@
-namespace DevelopersChallenge2Api {
-  using DevelopersChallenge2Api.Models;
-  using Microsoft.EntityFrameworkCore;
+namespace DevelopersChallenge2Api
+{
+    using DevelopersChallenge2Api.Models;
+    using Microsoft.EntityFrameworkCore;
 
-  public class ApplicationDatabaseContext : DbContext
-  {
-    public ApplicationDatabaseContext(DbContextOptions<ApplicationDatabaseContext> options)
-      : base(options) { }
+    public class ApplicationDatabaseContext : DbContext
+    {
+        public ApplicationDatabaseContext(DbContextOptions<ApplicationDatabaseContext> options)
+            : base(options) { }
 
-    public DbSet<Transaction> Transactions { get; set; }
-  }
+        public DbSet<Transaction> Transactions { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Transaction>()
+                .HasIndex(t => new { t.Timestamp, t.Description, t.Amount })
+                .IsUnique();
+        }
+    }
 }
--- DevelopersChallenge2Api/Models/Transaction.cs
+++ DevelopersChallenge2Api/Models/Transaction.cs
@@ -1,12 +1,20 @@
 namespace DevelopersChallenge2Api.Models
 {
-  using System;
-  using System.ComponentModel.DataAnnotations;
-  using Microsoft.EntityFrameworkCore;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
 
-  public class Transaction
-  {
-    [Key]
-    public int Id { get; set; }
-  }
+    public class Transaction
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public double Amount { get; set; }
+
+        public string Description { get; set; }
+
+        public string OperationType { get; set; }
+    }
 }
