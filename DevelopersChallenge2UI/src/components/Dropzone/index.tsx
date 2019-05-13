e541b30b295c3922876afebddb9416979ce53e1e
Rodrigo Saboya <saboya@users.noreply.github.com>
2019-05-13 00:38:13 -0300

    feature: adding useful callback props to Dropzone component

--- DevelopersChallenge2UI/src/components/Dropzone/index.tsx
+++ DevelopersChallenge2UI/src/components/Dropzone/index.tsx
@@ -9,19 +9,32 @@ require('./style.css')
 
 interface Props {
   className?: React.HTMLAttributes<any>['className'],
+  onFilesAccepted?: (acceptedFiles: File[]) => void,
+  onFilesRejected?: (rejectedFiles: File[]) => void,
+  onFilesAdded?: (files: File[]) => void,
 }
 
 const Component: React.FunctionComponent<Props> = (props) => {
   const [files, setFiles] = React.useState<File[]>([])
 
-  const onDrop = React.useCallback((acceptedFiles: File[]) => {
-    const filteredFiles = acceptedFiles.filter(file => file.name.endsWith('.ofx'))
-    setFiles(prevFiles => prevFiles.concat(filteredFiles))
+  const onDrop = React.useCallback((acceptedFiles: File[], rejectedFiles: File[]) => {
+    if (props.onFilesAccepted && acceptedFiles.length > 0) {
+      props.onFilesAccepted(acceptedFiles)
+    }
+
+    if (props.onFilesRejected && rejectedFiles.length > 0) {
+      props.onFilesRejected(rejectedFiles)
+    }
+
+    setFiles(prevFiles => prevFiles.concat(acceptedFiles))
   }, [])
 
-  React.useEffect(() => console.log(files), [files])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
+    accept: '.ofx',
+    onDrop,
+  })
 
-  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  React.useEffect(() => props.onFilesAdded && props.onFilesAdded(files), [files])
 
   return (
     <div>
